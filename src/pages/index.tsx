692ac281f66c476d675f5770ca368f37ad7a8da9
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-03-03 22:06:49 +0200

    render product list in app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
+import { Suspense } from "react";
+
 import DefaultLayout from "@/layouts/default";
-import { fetchProducts } from "@/services/api";
+import ProductList from "@/components/ProductList";
 
 export default function IndexPage() {
-  const products = fetchProducts();
-
-  console.log(products);
-
   return (
     <DefaultLayout>
-      <div className="flex flex-col items-center gap-6 text-center">
-        <p>welcome to joes store</p>
+      <div className="flex flex-col gap-6">
+        <h1 className="text-2xl font-bold text-center sm:text-left">
+          Welcome to Joe&apos;s Store
+        </h1>
+
+        <Suspense fallback={<p>Loading products...</p>}>
+          <ProductList />
+        </Suspense>
       </div>
     </DefaultLayout>
   );
