6fd5f50aee921561d231f97358f60bda891fedbd
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-03-04 09:24:42 +0200

    increse product count

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,7 @@ function mapProduct(apiProduct: ApiProduct): Product {
 }
 
 export async function fetchProducts(): Promise<Product[]> {
-  const res = await fetch("https://fakestoreapi.com/products?limit=8");
+  const res = await fetch("https://fakestoreapi.com/products?limit=50");
 
   if (!res.ok) {
     throw new Error("Failed to fetch products");
